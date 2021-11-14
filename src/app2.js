6519d75c8ad2530b47f46381ab59326cba982484
Hady Farahat <farhat2312@gmail.com>
2021-11-14 10:44:55 +0200

    secondry commit

---
+++ src/app2.js
@@ -0,0 +1,8 @@
+import React from "react";
+class app2 extends React.Component {
+  state = { Person: { Name: "", Bio: "", Imgsrc: "", ProfessionFinal: "" } };
+  render() {
+    return;
+  }
+}
+export default app2;
